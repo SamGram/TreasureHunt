4f1fc138d947604c499d798117af4c4d2dd73c3d
SamGram <mainlykarma@gmail.com>
2017-05-19 02:08:47 -0500

    Delete name.java

--- name.java
+++
@@ -1,9 +0,0 @@
-class name{
-	public static void main(String[] args){
-
-		System.out.println("Hello Youtube, my name is Sam Gram!");
-		System.out.print("A");
-		System.out.println("1.0");
-		System.out.print("I am 21 years old.");
-	}
-}
